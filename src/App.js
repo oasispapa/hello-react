710aff851eb70ef6730ae419bae4693e7d3b7400
oasispapa <oasispapa@gmail.com>
2023-12-20 17:10:10 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
 class App extends Component {
   render() {
-    return <ValidationSample />;
+    return (
+      <>
+        <ValidationSample />
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </>
+    );
   }
 }
 
